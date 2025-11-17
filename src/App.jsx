b2afffaf009cde9533ca97b2e3f5ce7a5d20f4b5
kbukowska <121160309+kbukowska@users.noreply.github.com>
2025-11-17 11:55:21 +0000

    Fix ESLint configuration and linting errors

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
 
   useEffect(() => {
     fetchCharacters()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, searchName, filters])
 
   const fetchCharacters = async () => {
